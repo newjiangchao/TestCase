ae0f23dd1200eb2211cc1acff6d65b2036ee1e75
newjiangchao <newjiangchao@gmail.com>
2024-11-16 13:04:47 +0800

    Added Spring Boot application context annotation

--- src/test/java/org/airwallex/test/EURToCNYTest.java
+++ src/test/java/org/airwallex/test/EURToCNYTest.java
@@ -6,15 +6,19 @@ import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.airwallex.Application;
 import org.airwallex.dto.ResponseBodyDTO;
 import org.airwallex.utils.CommonUtils;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class EURToCNYTest {
+@SpringBootTest(classes = Application.class)
+public class EURToCNYTest extends AbstractTestNGSpringContextTests {
 
     @Test(groups = {"smoke", "positive"}, testName = "Determine the amount of EUR exchanged for 100 CNY")
     public void EURToCNYTestCase1() {
